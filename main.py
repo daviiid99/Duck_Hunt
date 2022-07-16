ea8e162c1f3482f7860f2e125201d5b696632b46
daviiid99 <daviiideveloper@gmail.com>
2022-07-16 22:56:30 +0200

    Initial title screen

---
+++ main.py
@@ -0,0 +1,231 @@
+from resources import *
+import threading
+
+vec = pygame.math.Vector2
+
+def vec_to_int(vector):
+    return int(vector.x), int(vector.y)
+
+class Main :
+
+	def __init__(self) :
+		pygame.init()
+		self.running = True
+		self.clock = pygame.time.Clock()
+		self.screen = WIN
+		self.mouse = vec()
+		self.screen_rect = self.screen.get_rect()
+		self.press_to_start = True
+		self.count = 0
+
+
+	def draw_title_screen(self) :
+
+		while self.press_to_start :
+
+			movement_x = 50
+			movement_y = 250
+			dog_jump = 400
+			count = 0
+
+			# Dog walking // Duck flying
+
+			while count < 5 :
+
+				self.draw(walking_1, blue_horizontal_1, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(walking_2, blue_horizontal_1, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(walking_3, blue_horizontal_3, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(walking_4, blue_horizontal_3, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(walking_5, blue_horizontal_1, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(walking_6, blue_horizontal_2, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(walking_6, blue_horizontal_2, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(jump_1, blue_up_1, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				movement_y -= 10
+				dog_jump  -= 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(jump_1, blue_up_2, movement_x, movement_y, dog_jump)
+				self.clock.tick(10)
+				movement_x += 10
+				movement_y -= 10
+				dog_jump  -= 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(jump_1, blue_up_1, movement_x, movement_y, dog_jump)
+				self.clock.tick(30)
+				movement_x += 10
+				movement_y -= 10
+				dog_jump  -= 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(jump_1, blue_up_2, movement_x, movement_y, dog_jump)
+				self.clock.tick(30)
+				movement_x += 10
+				movement_y -= 10
+				dog_jump  -= 10
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(jump_2, blue_up_2, movement_x, movement_y, dog_jump)
+				self.clock.tick(30)
+				movement_x += 10
+				movement_y -= 10
+				dog_jump += 20
+				count +=1
+
+			count = self.count
+
+			while count < 5 :
+
+				self.draw(jump_2, blue_up_2, movement_x, movement_y, dog_jump)
+				self.clock.tick(30)
+				movement_x += 10
+				movement_y -= 10
+				dog_jump += 20
+				count +=1
+
+		
+	def draw(self, dog, duck, movement_x, movement_y, dog_jump) :
+		# Set background
+		self.screen.blit(title_screen_bg, (0,0))
+
+		# Title message
+		text = TITLE_FONT.render("Press to Start", 1, WHITE)
+		self.screen.blit(text, (270, 300))
+
+		# Draw dog
+		self.screen.blit(dog, (movement_x, dog_jump))
+
+		# Draw duck
+		self.screen.blit(duck, (movement_x, movement_y))
+
+		pygame.display.update()
+
+
+	def control_title_screen(self) :
+
+		while self.press_to_start : 
+
+			for event in pygame.event.get() :
+				if event.type == pygame.QUIT :
+					self.running = False
+					self.press_to_start = False
+
+				elif event.type == pygame.MOUSEBUTTONDOWN :
+					self.check_mouse(event.pos)
+
+	def check_mouse(self, mouse) :
+		if self.scree_rect.collidepoint(mouse) :
+			self.press_to_start = False
+
+	def play_music(self) :
+
+		count = 16
+
+		while self.press_to_start :
+
+			if count == 0 :
+				title.play()
+				count = 16
+
+			else :
+				count -=1
+
+
+	def start_title_screen(self) :
+
+		# Create new threads for title screen control
+		thread_1 = threading.Thread(target = self.draw_title_screen, name="ui")
+		thread_2 = threading.Thread(target = self.control_title_screen, name="mouse")
+		thread_3 = threading.Thread(target = self.play_music, name="sound")
+
+		# Start threads
+		thread_1.start()
+		thread_2.start()
+		thread_3.start()
+
+		# Wait for all threads to end
+		start = self.control_title_screen()
+
+		while self.press_to_start :
+			thread_1.join()
+			thread_2.join()
+			thread_3.join()
+
+
+
+main = Main()
+main.start_title_screen()
---
+++ resources.py
@@ -0,0 +1,126 @@
+import os
+import pygame
+import json
+import sys
+import ctypes
+
+
+## Game board
+pygame.init()
+width, height = 858, 540
+fpsClock = pygame.time.Clock()
+WIN = pygame.display.set_mode((width, height))
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (93.3,13.3,16.1)
+
+TITLE_FONT = pygame.font.SysFont('comicsans', 50)
+
+## Game Values
+FPS = 60
+MAX_CARDS = 108
+INITIAL_CARDS = 7
+MAX_TURN_TIME = 30
+clock = pygame.time.Clock()
+small_font = pygame.font.Font(None, 30)
+count_font = pygame.font.Font(None, 50)
+mini_font = pygame.font.Font(None, 18)
+
+
+## App Info
+icon = pygame.image.load('Assets/logo/logo.png')
+pygame.display.set_icon(icon)
+pygame.display.set_caption("Duck Hunt")
+
+
+# Game Assets
+
+# Title Screen
+title_screen_bg = pygame.transform.scale(pygame.image.load(os.path.join('Assets/background/title_screen', "background.png")), (858, 540))
+
+# Duck Hunt
+
+# Walking
+walking_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/walking', 'walking_1.png')), (150, 150))
+walking_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/walking', 'walking_2.png')), (150, 150))
+walking_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/walking', 'walking_3.png')), (150, 150))
+walking_4 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/walking', 'walking_4.png')), (150, 150))
+walking_5 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/walking', 'walking_5.png')), (150, 150))
+walking_6 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/walking', 'walking_6.png')), (150, 150))
+
+# Jump
+jump_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/jump', 'jump_1.png')), (150, 150))
+jump_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/dog/jump', 'jump_2.png')), (150, 150))
+
+# Ducks
+
+# Blue
+
+# Horizontal
+blue_horizontal_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/horizontal', 'horizontal.png')), (120, 120))
+blue_horizontal_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/horizontal', 'horizontal_2.png')), (120, 120))
+blue_horizontal_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/horizontal', 'horizontal_3.png')), (120, 120))
+
+# Up
+blue_up_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/up', 'up.png')), (120, 120))
+blue_up_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/up', 'up_2.png')), (120, 120))
+blue_up_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/up', 'up_3.png')), (120, 120))
+
+# Fall
+blue_falling_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/falling', 'fall.png')), (120, 120))
+blue_falling_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/falling', 'fall_2.png')), (120, 120))
+blue_falling_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/falling', 'fall_3.png')), (120, 120))
+blue_falling_4 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/falling', 'fall_4.png')), (120, 120))
+
+# Shoot
+blue_shoot = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/blue/shoot', 'shoot.png')), (120, 120))
+
+
+# Green
+
+# Horizontal
+green_horizontal_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/horizontal', 'horizontal.png')), (120, 120))
+green_horizontal_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/horizontal', 'horizontal_2.png')), (120, 120))
+green_horizontal_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/horizontal', 'horizontal_3.png')), (120, 120))
+
+# Up
+green_up_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/up', 'up.png')), (120, 120))
+green_up_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/up', 'up_2.png')), (120, 120))
+green_up_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/up', 'up_3.png')), (120, 120))
+
+# Fall
+green_falling_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/falling', 'fall.png')), (120, 120))
+green_falling_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/falling', 'fall_2.png')), (120, 120))
+green_falling_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/falling', 'fall_3.png')), (120, 120))
+green_falling_4 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/falling', 'fall_4.png')), (120, 120))
+
+# Shoot
+green_shoot = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/green/shoot', 'shoot.png')), (120, 120))
+
+
+# Brown
+
+# Horizontal
+brown_horizontal_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/horizontal', 'horizontal.png')), (120, 120))
+brown_horizontal_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/horizontal', 'horizontal_2.png')), (120, 120))
+brown_horizontal_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/horizontal', 'horizontal_3.png')), (120, 120))
+
+# Up
+brown_up_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/up', 'up.png')), (120, 120))
+brown_up_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/up', 'up_2.png')), (120, 120))
+brown_up_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/up', 'up_3.png')), (120, 120))
+
+# Fall
+brown_falling_1 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/falling', 'fall.png')), (120, 120))
+brown_falling_2 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/falling', 'fall_2.png')), (120, 120))
+brown_falling_3 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/falling', 'fall_3.png')), (120, 120))
+brown_falling_4 = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/falling', 'fall_4.png')), (120, 120))
+
+# Shoot
+brown_shoot = pygame.transform.scale(pygame.image.load(os.path.join('assets/npc/duck/brown/shoot', 'shoot.png')), (120, 120))
+
+
+# Sound effect
+title = pygame.mixer.Sound("assets/music/title.mp3")
+gotcha = pygame.mixer.Sound("assets/music/gotcha.mp3")
